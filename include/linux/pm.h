a5f931467fd30306e6e0f54759c4e8172493a1df
Rafael J. Wysocki <rjw@sisk.pl>
2012-03-04 23:11:14 +0100

    Merge branch 'pm-sleep'
    * pm-sleep:
      PM / Freezer: Remove references to TIF_FREEZE in comments
      PM / Sleep: Add more wakeup source initialization routines
      PM / Hibernate: Enable usermodehelpers in hibernate() error path
      PM / Sleep: Make __pm_stay_awake() delete wakeup source timers
      PM / Sleep: Fix race conditions related to wakeup source timer function
      PM / Sleep: Fix possible infinite loop during wakeup source destruction
      PM / Hibernate: print physical addresses consistently with other parts of kernel
      PM: Add comment describing relationships between PM callbacks to pm.h
      PM / Sleep: Drop suspend_stats_update()
      PM / Sleep: Make enter_state() in kernel/power/suspend.c static
      PM / Sleep: Unify kerneldoc comments in kernel/power/suspend.c
      PM / Sleep: Remove unnecessary label from suspend_freeze_processes()
      PM / Sleep: Do not check wakeup too often in try_to_freeze_tasks()
      PM / Sleep: Initialize wakeup source locks in wakeup_source_add()
      PM / Hibernate: Refactor and simplify freezer_test_done
      PM / Hibernate: Thaw kernel threads in hibernation_snapshot() in error/test path
      PM / Freezer / Docs: Document the beauty of freeze/thaw semantics
      PM / Suspend: Avoid code duplication in suspend statistics update
      PM / Sleep: Introduce generic callbacks for new device PM phases
      PM / Sleep: Introduce "late suspend" and "early resume" of devices
    
    Conflicts:
    
    	kernel/power/hibernate.c

--- include/linux/pm.h
+++ include/linux/pm.h
@@ -320,6 +320,15 @@ const struct dev_pm_ops name = { \
 /*
  * Use this for defining a set of PM operations to be used in all situations
  * (sustem suspend, hibernation or runtime PM).
+ * NOTE: In general, system suspend callbacks, .suspend() and .resume(), should
+ * be different from the corresponding runtime PM callbacks, .runtime_suspend(),
+ * and .runtime_resume(), because .runtime_suspend() always works on an already
+ * quiescent device, while .suspend() should assume that the device may be doing
+ * something when it is called (it should ensure that the device will be
+ * quiescent after it has returned).  Therefore it's better to point the "late"
+ * suspend and "early" resume callback pointers, .suspend_late() and
+ * .resume_early(), to the same routines as .runtime_suspend() and
+ * .runtime_resume(), respectively (and analogously for hibernation).
  */
 #define UNIVERSAL_DEV_PM_OPS(name, suspend_fn, resume_fn, idle_fn) \
 const struct dev_pm_ops name = { \
@@ -608,17 +617,23 @@ extern void __suspend_report_result(const char *function, void *fn, int ret);
 extern int device_pm_wait_for_dev(struct device *sub, struct device *dev);
 
 extern int pm_generic_prepare(struct device *dev);
+extern int pm_generic_suspend_late(struct device *dev);
 extern int pm_generic_suspend_noirq(struct device *dev);
 extern int pm_generic_suspend(struct device *dev);
+extern int pm_generic_resume_early(struct device *dev);
 extern int pm_generic_resume_noirq(struct device *dev);
 extern int pm_generic_resume(struct device *dev);
 extern int pm_generic_freeze_noirq(struct device *dev);
+extern int pm_generic_freeze_late(struct device *dev);
 extern int pm_generic_freeze(struct device *dev);
 extern int pm_generic_thaw_noirq(struct device *dev);
+extern int pm_generic_thaw_early(struct device *dev);
 extern int pm_generic_thaw(struct device *dev);
 extern int pm_generic_restore_noirq(struct device *dev);
+extern int pm_generic_restore_early(struct device *dev);
 extern int pm_generic_restore(struct device *dev);
 extern int pm_generic_poweroff_noirq(struct device *dev);
+extern int pm_generic_poweroff_late(struct device *dev);
 extern int pm_generic_poweroff(struct device *dev);
 extern void pm_generic_complete(struct device *dev);
 
