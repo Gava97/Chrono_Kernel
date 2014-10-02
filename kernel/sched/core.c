c9b6bf5f7d59292be67d3595b5c54953553050eb
Marcin Kaluza <marcin.kaluza@trioptimum.com>
2014-10-02 16:45:18 +0200

    export symbols necessary to build cpu governors as modules

--- kernel/sched/core.c
+++ kernel/sched/core.c
@@ -2149,6 +2149,7 @@ unsigned long nr_running(void)
 
 	return sum;
 }
+EXPORT_SYMBOL_GPL(nr_running);
 
 unsigned long nr_uninterruptible(void)
 {
@@ -4410,6 +4411,7 @@ int sched_setscheduler_nocheck(struct task_struct *p, int policy,
 {
 	return __sched_setscheduler(p, policy, param, false);
 }
+EXPORT_SYMBOL_GPL(sched_setscheduler_nocheck);
 
 static int
 do_sched_setscheduler(pid_t pid, int policy, struct sched_param __user *param)
