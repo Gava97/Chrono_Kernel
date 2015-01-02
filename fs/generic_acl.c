1e15cc14ca186cd19607fa4ac0dcb48f04b77b10
Shilin Victor <chrono.monochrome@gmail.com>
2015-01-02 18:44:41 +0400

    fs: generic_acl: revert changes
    Signed-off-by: Shilin Victor <chrono.monochrome@gmail.com>

--- fs/generic_acl.c
+++ fs/generic_acl.c
@@ -190,7 +190,7 @@ generic_acl_chmod(struct inode *inode)
 }
 
 int
-generic_check_acl(struct inode *inode, int mask)
+generic_check_acl(struct inode *inode, int mask, unsigned int flags)
 {
 	if (flags & IPERM_FLAG_RCU) {
 		if (!negative_cached_acl(inode, ACL_TYPE_ACCESS))
